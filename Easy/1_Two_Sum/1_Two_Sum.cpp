4eb1314b7f5a5ca3a11e261a20d42e3d6b149889
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-05 01:55:35 -0500

    solved O(n^2) twosum

--- Easy/1_Two_Sum/1_Two_Sum.cpp
+++ Easy/1_Two_Sum/1_Two_Sum.cpp
@@ -9,7 +9,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        vector<int> answer;
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums.size() && j != i; i++) {
+                if(nums[i] + nums[j] == target) {
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    return answer;
+                }
+            }
+        }
+        return answer;
     }
 };
 
