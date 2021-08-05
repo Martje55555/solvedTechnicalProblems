b32967470b8a6a36b62b793e10b767f07b2f0fa2
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-05 01:53:35 -0500

    added easy twosome num 1

---
+++ Easy/1_Two_Sum/1_Two_Sum.cpp
@@ -0,0 +1,17 @@
+#include "iostream";
+#include "iomanip";
+#include "vector";
+
+using namespace std;
+
+// First Solution
+// O(n^2)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+//Follow up todo:
+// comeup with Less than O(n^2) solution
