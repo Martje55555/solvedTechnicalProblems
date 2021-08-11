52ee4112e142a5c90ebcc69030e6f99cf27156d0
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-08-11 00:44:50 -0500

    added new solutions to problem 1920

--- LeetCode/C++/Easy/1920_BuildArrayFromPermutation/O(1)Space_solution.cpp
+++ LeetCode/C++/Easy/1920_BuildArrayFromPermutation/O(1)Space_solution.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
-
-// solution is O(n) linear, as it has to iterate through
-// each element in the array.
-
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans;
+        int n = nums.size();
         for(int i = 0; i < nums.size(); i++) {
-            ans.push_back(nums[nums[i]]);
+            nums[i] = nums[i] + n*(nums[nums[i]]%n);
         }
-        return ans;
+        for(int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+        return nums;
     }
 };
