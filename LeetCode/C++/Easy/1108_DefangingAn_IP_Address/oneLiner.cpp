42089439a81a8730226320dcfb7a1ab33645e30f
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-09-04 14:52:18 -0500

    defang ip address one liner solution

---
+++ LeetCode/C++/Easy/1108_DefangingAn_IP_Address/oneLiner.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <regex>
+
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        return regex_replace(address, regex("[.]"), "[.]");
+    }
+};
