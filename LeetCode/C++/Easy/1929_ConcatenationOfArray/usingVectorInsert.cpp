b00166e92fb8105e9cbadf1ec4e9484a0c55eada
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-09-04 14:18:28 -0500

    added solution to easy 1929

---
+++ LeetCode/C++/Easy/1929_ConcatenationOfArray/usingVectorInsert.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> answer(nums);
+        answer.insert(answer.end(), nums.begin(), nums.end());
+        return answer;
+    }
+};
