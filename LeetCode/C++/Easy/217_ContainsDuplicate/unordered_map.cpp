269e7dec6b8a247a18461cc06155bdcf7ee4932c
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2021-09-11 11:55:21 -0500

    adding 217

---
+++ LeetCode/C++/Easy/217_ContainsDuplicate/unordered_map.cpp
@@ -0,0 +1,18 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
+            if(map[nums[i]] > 1) return true;
+        }
+
+        return false;
+    }
+};
